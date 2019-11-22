0062b34e8d681542aa12aaf8e9c4b78866896700
Kyle Cribbs <kylecribbs@outlook.com>
2019-11-22 15:38:50 -0700

    bump: version 2.1.0 → 2.2.0

--- pyarcher/__init__.py
+++ pyarcher/__init__.py
@@ -6,7 +6,7 @@ from pyarcher.archer import Archer
 
 __author__ = """Kyle Cribbs"""
 __email__ = 'kylecribbs@outlook.com'
-__version__ = '2.1.0'
+__version__ = '2.2.0'
 __all__ = ["Archer"]
 
 name = "pyarcher"
