bb7327a1b4b994f9d09ec0afa9d4205cfd243751
Kyle Cribbs <kylecribbs@outlook.com>
2019-11-22 16:13:30 -0700

    fix(Setup): Fixed install

--- setup.py
+++ setup.py
@@ -13,7 +13,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+try:
+    requirements = open('requirements.txt').readlines()
+except FileNotFoundError:
+    requirements = []
 
 setup_requirements = ['pytest-runner', ]
 
